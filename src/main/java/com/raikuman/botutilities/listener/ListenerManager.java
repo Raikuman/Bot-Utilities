a9a91eaf681b5b15226b37091446e0081b6e9abe
Raikuman <40256283+Raikuman@users.noreply.github.com>
2022-06-23 19:13:25 -0700

    Method to add ListenerAdapters to ListenerBuilder

--- src/main/java/com/raikuman/botutilities/listener/ListenerManager.java
+++ src/main/java/com/raikuman/botutilities/listener/ListenerManager.java
@@ -6,20 +6,23 @@ import com.raikuman.botutilities.commands.CommandEventListener;
 import com.raikuman.botutilities.commands.manager.CommandInterface;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * A manager to provide the listeners to the JDA object
  *
- * @version 1.0 2022-19-06
+ * @version 1.1 2022-23-06
  * @since 1.0
  */
 public class ListenerManager {
 
+	private final List<ListenerAdapter> listeners;
 	private final List<CommandInterface> commands;
 	private final List<ButtonInterface> buttons;
 
-	ListenerManager(List<CommandInterface> commands, List<ButtonInterface> buttons) {
+	ListenerManager(List<ListenerAdapter> listeners, List<CommandInterface> commands, List<ButtonInterface> buttons) {
+		this.listeners = listeners;
 		this.commands = commands;
 		this.buttons = buttons;
 	}
@@ -29,10 +32,13 @@ public class ListenerManager {
 	 * @return The array of listeners
 	 */
 	public Object[] getListeners() {
-		return new ListenerAdapter[] {
-			getCommandListener(),
-			getButtonListener()
-		};
+		List<Object> listenerList = new ArrayList<>();
+		listenerList.add(getCommandListener());
+		listenerList.add(getButtonListener());
+		listenerList.addAll(listeners);
+
+
+		return listenerList.toArray();
 	}
 
 	/**
