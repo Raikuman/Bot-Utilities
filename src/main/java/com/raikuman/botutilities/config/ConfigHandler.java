84df63590e756532d193848730f54ddb479b5f68
Raikuman <40256283+Raikuman@users.noreply.github.com>
2023-12-21 22:43:03 -0800

    Update config creation

--- src/main/java/com/raikuman/botutilities/config/ConfigHandler.java
+++ src/main/java/com/raikuman/botutilities/config/ConfigHandler.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
+import java.util.List;
 import java.util.Map;
 
 public class ConfigHandler {
@@ -11,12 +12,24 @@ public class ConfigHandler {
     private static final Logger logger = LoggerFactory.getLogger(ConfigHandler.class);
     private static final String DEFAULT_CONFIG = "config" + File.separator;
 
-    public static void writeConfigs(Config[] configs) {
+    public static void writeConfigs(List<Config> configs) {
         for (Config config : configs) {
             File configFile = new File(DEFAULT_CONFIG + config.fileName() + ".cfg");
 
             // Config exists...
             if (configFile.exists()) continue;
+
+            // Create config
+            try {
+                if (configFile.getParentFile().mkdirs() && configFile.createNewFile()) {
+                    logger.info("Config file \"" + config.fileName() + "\" created");
+                } else {
+                    logger.error("Could not create config file \"" + config.fileName() + "\" created");
+                }
+            } catch (IOException e) {
+                logger.error("Error creating config file \"" + config.fileName() + "\"");
+                continue;
+            }
 
             try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(configFile))) {
                 for (Map.Entry<String, String> entry : config.configs().entrySet()) {
