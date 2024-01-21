cf688bd0ed01bc0d3f6552eefe7ae9726aa85e0b
Raikuman <40256283+Raikuman@users.noreply.github.com>
2024-01-21 14:26:46 -0800

    Add embed response for command usage

--- src/main/java/com/raikuman/botutilities/utilities/EmbedResources.java
+++ src/main/java/com/raikuman/botutilities/utilities/EmbedResources.java
@@ -1,5 +1,6 @@
 package com.raikuman.botutilities.utilities;
 
+import com.raikuman.botutilities.defaults.database.DefaultDatabaseHandler;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
@@ -15,6 +16,18 @@ public class EmbedResources {
 
     public static EmbedBuilder success(String title, String description, MessageChannelUnion channel, User user) {
         return defaultResponse("#04D500", title, "✅ " + description, channel, user);
+    }
+
+    public static EmbedBuilder incorrectUsage(String invoke, String usage, MessageChannelUnion channel) {
+        String prefix = DefaultDatabaseHandler.getPrefix(channel.asGuildMessageChannel().getGuild());
+
+        return defaultResponse(
+            "#C10000",
+            "Incorrect usage for " + prefix + invoke,
+            "Usage: `" + prefix + invoke + " " + usage + "`",
+            channel,
+            channel.getJDA().getSelfUser()
+        );
     }
 
     private static EmbedBuilder defaultResponse(String color, String title, String description,
